e556dfd2a7c3b04715d3a90c5b731a1fd74cf33b
Aya <ayafawzimhmod@gmail.com>
2020-05-02 21:04:13 +0200

    Revert "Added homeScreen"
    This reverts commit e0f9d3061407048e7e5fcbd19d24bff8e788d604.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:ieeeapp/screens/home_page.dart';
 import 'package:ieeeapp/screens/splash_screen.dart';
 
 void main() => runApp(MaterialApp(
-      title: "IEEE APP",
-      initialRoute: HomeScreen.id,
-      routes: {
-        SplashScreen.id: (context) => SplashScreen(),
-        HomeScreen.id: (context) => HomeScreen(),
-      },
-      debugShowCheckedModeBanner: false,
-    ));
+  title: "IEEE APP",
+  initialRoute: SplashScreen.id ,
+  routes: {
+    SplashScreen.id : (context) => SplashScreen(),
+
+  },
+));
+
