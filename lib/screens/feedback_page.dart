944b0035bfedc6259d39b4edd3c620d943a035ff
Nada161 <60591589+Nada161@users.noreply.github.com>
2020-06-23 01:51:50 +0200

    update feedback

--- lib/screens/feedback_page.dart
+++ lib/screens/feedback_page.dart
@@ -8,27 +8,39 @@ class FeedbackPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title:Text('Feedback'),centerTitle: true),
-        body:Container(
-          child: ListView.builder(
-              itemCount: nHelper.feedBack.length,
-              itemBuilder: (context, index) {
+      appBar: AppBar(title: Text('Feedback'), centerTitle: true),
+      body: Container(
+        child: ListView.builder(
+            itemCount: nHelper.feedBack.length,
+            itemBuilder: (context, index) {
+              if (nHelper.feedBack.isEmpty) {
+                return Center(
+                  child: Text(
+                    "You Don't have Feedback yet!",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                );
+              } else {
                 return Card(
                   margin: EdgeInsets.all(10.0),
-                    child:
-                          Expanded(
-                              child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Text("${nHelper.feedBack[index].fedTit}",style: TextStyle(fontWeight: FontWeight.bold),),
-                                    Text("${nHelper.feedBack[index].fedBody}"),
-                                    Text("${nHelper.feedBack[index].fedDate}"),
-                                  ])),
-                        );
-              }),
-        ),
-
-
+                  child: Expanded(
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                        Text(
+                          "${nHelper.feedBack[index].fedTit}",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text("${nHelper.feedBack[index].fedBody}"),
+                        Text("${nHelper.feedBack[index].fedDate}"),
+                      ])),
+                );
+              }
+            }),
+      ),
     );
   }
 }
